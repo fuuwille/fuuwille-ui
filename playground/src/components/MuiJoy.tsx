92edf23b47507bfe6d4d1173ba1e8be0dc4b1892
k4yr2 <kyrzkaya@gmail.com>
2025-06-08 19:16:44 +0300

    feat: add MuiJoy component with CssVarsProvider and CssBaseline

---
+++ playground/src/components/MuiJoy.tsx
@@ -0,0 +1,13 @@
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
+
+export const MuiJoy = () => {
+    return (
+        <>
+            <CssVarsProvider>
+                <CssBaseline />
+            </CssVarsProvider>
+        </>
+    );
+}
+
+export default MuiJoy;
