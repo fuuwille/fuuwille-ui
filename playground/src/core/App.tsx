79263d523de611533b08fc22692fb6bfffa2fe5e
k4yr2 <kyrzkaya@gmail.com>
2025-06-08 19:16:47 +0300

    feat: integrate MuiJoy component into App

--- playground/src/core/App.tsx
+++ playground/src/core/App.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header"
+import { MuiJoy } from "../components/MuiJoy"
 
 function App() {
 
   return (
     <>
       <Header />
+      <MuiJoy />
     </>
   )
 }
