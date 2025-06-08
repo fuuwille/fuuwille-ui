5cad00675ac94f0bfd38f9aed79adbca9b50e129
k4yr2 <kyrzkaya@gmail.com>
2025-06-08 19:56:46 +0300

    feat: add dark mode toggle functionality to Header component

--- playground/src/components/Header.tsx
+++ playground/src/components/Header.tsx
@@ -1,9 +1,20 @@
+import { Switch } from '@mui/joy';
 import style from '../styles/Header.module.css';
+import { useAppTheme } from '../core/main';
 
 export const Header = () => {
+    const { mode, toggleMode } = useAppTheme((state) => state);
+
     return (
         <header className={style.header}>
-
+            <Switch
+                checked={mode === "dark"}
+                onChange={toggleMode}
+                slotProps={{
+                    input: { 'aria-label': 'Toggle dark mode' },
+                }}
+                sx={{ marginLeft: 'auto' }}
+            />
         </header>
     );
 }
